1979d01a6d0468f9cbc71fdd81fd24b7aa10acd3
Feraz Khan <ferazahmadgit62@gmail.com>
2022-10-31 01:13:13 +0530

    login/signup page

---
+++ src/components/Login.js
@@ -0,0 +1,112 @@
+import React, { useRef, useState } from "react";
+
+import "./Login.css";
+
+const Login = () => {
+  const [login, setLogin] = useState(false);
+  const InputEmail = useRef();
+  const InputPassword = useRef();
+  const InputConfirmPassword = useRef();
+  const modeChangeHandler = () => {
+    setLogin((prevState) => !prevState);
+  };
+  const authHandler = async () => {
+    setLogin(false);
+    let email = InputEmail.current.value;
+    let password = InputPassword.current.value;
+
+    if (!login) {
+      let confirmPassword = InputConfirmPassword.current.value;
+      if (!email || !password || !confirmPassword) {
+        alert("please fill the all fields");
+        return;
+      } else {
+        if (password !== confirmPassword) {
+          alert("password and confirm password are not matching");
+          return;
+        }
+        try {
+          const response = await fetch(
+            "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDQCWs9SI07zAWukmPD860hEMGb4ks4E8o",
+            {
+              method: "POST",
+              body: JSON.stringify({
+                email: email,
+                password: password,
+                returnSecureToken: true,
+              }),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          if (response.ok) {
+            setLogin(true);
+            InputConfirmPassword.current.value =
+              InputEmail.current.value =
+              InputPassword.current.value =
+                "";
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+    } else {
+      try {
+        const response = await fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDQCWs9SI07zAWukmPD860hEMGb4ks4E8o",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email: email,
+              password: password,
+              returnSecureToken: true,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        const data = await response.json();
+        console.log(data);
+        if (response.ok) {
+          console.log("user login success fully");
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="signup">
+        <div className="signup__title">
+          <h1>Signup</h1>
+        </div>
+        <div className="signup__inputs">
+          <input placeholder="Email" ref={InputEmail} type="text" />
+          <input placeholder="Password" ref={InputPassword} />
+          {!login && (
+            <input placeholder="Confirm Password" ref={InputConfirmPassword} />
+          )}
+          {
+            <button onClick={authHandler} className="signup__btn">
+              {`${!login ? "SignUp" : "Login"}`}
+            </button>
+          }
+        </div>
+      </div>
+      <div className="modechange">
+        {!login && (
+          <button onClick={modeChangeHandler} className="login__btn">
+            Have a account?Login
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
